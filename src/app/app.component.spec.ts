ae2e70218df889348353ef933e9c855490711e77
DESKTOP-FC5JKIA\pelle <pellejero44@gmail.com>
2020-09-01 01:00:30 +0200

    lint changes

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,54 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
 import { AppComponent } from './app.component';
+import { State } from './store/reducers/auth.reducers';
+import { AuthService } from './services/implementations/auth.service';
+import { AuthServiceMock } from './mockers/auth.service-mock';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let mockStore: MockStore;
+
   beforeEach(async(() => {
+    const initialState: State = {
+      isAuthenticated: false,
+      user: null,
+      errorMessageLogin: null,
+      canCloseLoginView: null,
+      errorMessageSignUp: null,
+      canCloseSignUpView: null
+    };
+    
     TestBed.configureTestingModule({
       imports: [
         RouterTestingModule
       ],
       declarations: [
         AppComponent
+      ], providers: [
+        { provide: AuthService, useClass: AuthServiceMock },
+        provideMockStore({ initialState })
       ],
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    mockStore = TestBed.inject(MockStore);
+    fixture.detectChanges();
   }));
 
-  // it('should create the app', () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   const app = fixture.componentInstance;
-  //   expect(app).toBeTruthy();
-  // });
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
 
-  // it(`should have as title 'star-wars-angel'`, () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   const app = fixture.componentInstance;
-  //   expect(app.title).toEqual('star-wars-angel');
-  // });
+  it('should check if user is logged in on component Init', () => {
+    spyOn(mockStore, 'dispatch').and.callThrough();
+    component.ngOnInit();
+    expect(mockStore.dispatch).toHaveBeenCalledTimes(1);
+  });
 
-  // it('should render title', () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   fixture.detectChanges();
-  //   const compiled = fixture.nativeElement;
-  //   expect(compiled.querySelector('.content span').textContent).toContain('star-wars-angel app is running!');
-  // });
 });
